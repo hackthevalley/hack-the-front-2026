67815ab4c41c8c26d118dad37fc56819fbd42357
Samuel Tjhia <110407645+samtjhia@users.noreply.github.com>
2026-05-26 17:37:16 -0400

    clean up and split LandingBackground into multiple files

---
+++ app/landing/background/rules.ts
@@ -0,0 +1,14 @@
+export const CORE_WIDE_SWAP_IDS = new Set([
+  "trees-5",
+  "front-trees",
+]);
+
+export const CORE_WIDE_WATER_SWAP_IDS = new Set([
+  "lots-of-spray",
+  "lots-of-spray-2",
+  "waterfall-glow",
+  "side-fall",
+  "waterfall",
+  "water-details",
+  "left-mountain",
+]);
