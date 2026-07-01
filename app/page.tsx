c0b793d825f0438e0be56b3c458bee251e81d9fc
Samuel Tjhia <110407645+samtjhia@users.noreply.github.com>
2026-07-01 15:01:12 -0400

    Sam homepage bg (#7)
    * home page foundation
    
    * add stumps and visual bounds
    
    * add back leaves and grass
    
    * add more leaves, mushroom, rock, sparkles
    
    * add path, stats, and fix cropping

--- app/page.tsx
+++ app/page.tsx
@@ -1,22 +1,9 @@
-import LandingBackground from "./landing/background/LandingBackground";
-import NewsletterSignUp from "@/components/ui/NewsletterSignUp";
+import HomeBackground from "./home/background/HomeBackground";
 
 export default function Home() {
   return (
-    <main
-      className="relative min-h-screen w-full overflow-hidden"
-      style={{
-        background:
-          "linear-gradient(180deg, #040142 15.36%, #C541E0 59.31%, #DF63DC 71.52%, #FFD668 95.53%)",
-      }}
-    >
-      <LandingBackground />
-
-      <section className="relative w-full min-h-screen">
-        <div className="absolute left-1/2 top-[46.9%] -translate-x-1/2">
-          <NewsletterSignUp />
-        </div>
-      </section>
+    <main className="relative w-full overflow-x-hidden">
+      <HomeBackground />
     </main>
   );
 }
