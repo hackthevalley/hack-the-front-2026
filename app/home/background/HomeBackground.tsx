bf648ea3c3139f9dc00b090cbc7ca8abc339102e
Andy Yin <andyyin06@gmail.com>
2026-07-12 17:29:13 -0400

    fix: More background changes

--- app/home/background/HomeBackground.tsx
+++ app/home/background/HomeBackground.tsx
@@ -2,6 +2,7 @@ import type { ReactNode } from "react";
 import HomeNavbar from "@/components/layout/HomeNavbar";
 import Button from "@/components/ui/Button";
 
+import type { ReactNode } from "react";
 import {
   HOME_BACKGROUND_DESIGN_HEIGHT,
   HOME_BACKGROUND_DESIGN_WIDTH,
