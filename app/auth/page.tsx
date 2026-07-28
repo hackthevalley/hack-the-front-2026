f7aaf20b06ce823d0f8dcc4fd6318a527bce0c0c
Andy Yin <andyyin06@gmail.com>
2026-07-28 14:41:32 -0400

    feat: nav bar

--- app/auth/page.tsx
+++ app/auth/page.tsx
@@ -1,9 +1,12 @@
+import HomeNavbar from "@/components/layout/HomeNavbar";
 import AuthBackground from "./background/AuthBackground";
 
 export default function AuthPage() {
   return (
     <main>
-      <AuthBackground />
+      <AuthBackground>
+        <HomeNavbar showSocialLinks={false} />
+      </AuthBackground>
     </main>
   );
 }
